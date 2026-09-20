956f5f0584e599c0befde6512294b3a1cbcab1b7
SimpleBlog <934192776@qq.com>
2026-09-21 09:36:16 +1200

    update requirements and 加注释

--- AppointmentSystem/urls.py
+++ AppointmentSystem/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admin 仅用于开发和测试，不作为系统的管理员界面
+    # 管理员界面是自定义的 /admin-dashboard/
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
